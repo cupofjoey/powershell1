20d2468f0cbd94b7dd017aa9023a4fc05c6ff171
Joe Hill <joe@jkh3.com>
2017-08-16 23:39:29 -0400

    Attempt to write a Pshell script that recognizes file names that are actually DayDate

---
+++ Option 1.ps1
@@ -0,0 +1,6 @@
+﻿$RetainedDays = 1
+$culture = [System.Globalization.CultureInfo]::InvariantCulture
+$ProcessFiles = Get-ChildItem - Path $RootPath -Recurse | Select FullName,@{
+L='ParsedDate';
+E={ [DateTime]::ParseExact($_.Name"'app_'MMddyyyy.log'",$culture)}
+} | Where { $_.ParsedDate -lt (Get-Date).AddDays(-$RetainedDays)}
---
+++ Option 2.ps1
@@ -0,0 +1,5 @@
+﻿$RetainedDays = 1
+$ArchiveBoundary = (Get-Date).AddDays(-$RetainedDays).ToString('MMddyyyy')
+
+$ProcessFiles = Get-ChildItem -Path $RootPath -Recurse | 
+Where-Object { ($_.BaseName -split '_|\.')[1] -lt $ArchiveBoundary
